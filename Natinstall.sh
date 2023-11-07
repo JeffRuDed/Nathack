065c1cf5ed7cf232a8c594c03f7cbb33fa365d05
JeffRuDed <125977468+JeffRuDed@users.noreply.github.com>
2023-11-07 17:55:20 +0300

    Update

--- Natinstall.sh
+++ Natinstall.sh
@@ -1,5 +1,4 @@
 #!/data/data/com.termux/files/usr/bin/bash
-
 cd $HOME
 echo
 echo -e "\e[93mThis script will install Nathack in Termux."
@@ -8,21 +7,24 @@ echo -e "\e[32m[*] \e[34mDownloading..."
 termux-setup-storage -y
 apt install python3 -y
 apt install megacmd -y
-apt install git -y
+if [ ! -d "Nathack" ]; then
+  apt install git -y
+fi
 pip install pystyle
 pip install requests
 git clone "https://github.com/JeffRuDed/Nathack"
+clear
 echo -e "\e[32m[*] \e[34mKeep downloading.."
 cd Nathack
-mega-get "https://mega.nz/file/mOwQTBAD#8jbpPYRbHDhJx8n5z1gR4ovf5JFtZrzqsPhROLD_L_A"
+echo -e "\e[32m[*] \e[34mSetting Permissions..."
 cd $HOME/Nathack
-echo -e "\e[32m[*] \e[34mCreating workspace directory..."
-cd $HOME
-cd Nathack
 chmod +x main.py
-echo -e "\e[32m[*] \e[34mCleaning up..."
-rm -rf NatInstall.sh
-echo
+echo -e "\e[32m[*] \e[34m Finishing up..."
+# SCRIPT BY @OSLEP FOR NATNONI
+if [ ! -f "instagram.txt" ]; then
+  mega-get "https://mega.nz/file/mOwQTBAD#8jbpPYRbHDhJx8n5z1gR4ovf5JFtZrzqsPhROLD_L_A"
+fi
 echo -e "\e[32mNathack was successfully installed!\e[39m"
 echo
+clear
 python3 main.py
